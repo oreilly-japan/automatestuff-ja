de60d5cd22dfa0dd75077b48f4c4675c78e4c2c2
aizoaikawa <aizoaikawa@gmail.com>
2017-07-16 15:42:27 +0900

    Twilioの更新
    TwilioRestClientからClientに変更

--- ch16/textmyself.py
+++ ch16/textmyself.py
@@ -7,8 +7,9 @@ auth_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 twilio_number = '+12345678901' # 購入した米国の電話番号
 my_number = '+819012345678'   # 自分の携帯電話番号
 
-from twilio.rest import TwilioRestClient
+try:from twilio.rest import TwilioRestClient
+except: from twilio.rest import Client as TwilioRestClient
 
-def textmyself(message):  # ?
-    twilio_cli = TwilioRestClient(account_SID, auth_token)  # ?
+def textmyself(message):
+    twilio_cli = TwilioRestClient(account_SID, auth_token)
     twilio_cli.messages.create(body=message, from_=twilio_number, to=my_number)  # ?
